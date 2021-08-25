cb5ecf41088c48283dc0f23d6d370191a7862fd1
Çetin ARDAL <kral2@users.noreply.github.com>
2021-08-25 18:25:16 +0200

    chore: update Terraform required minimum version

---
+++ docs/release-notes/relnote-v0.2.1.md
@@ -0,0 +1,3 @@
+# Changes
+
+- Terraform requirement is now version 1.0.0 or greater
--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ Description: Define Terraform cli and provider versions, Configure providers
 */
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
   required_providers {
     oci = {
       version = ">=4.0.0"
